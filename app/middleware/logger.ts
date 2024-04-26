67c7774b094ebc4a538a549dc39d4cbf05d01763
liujie <572890972@qq.com>
2024-04-26 15:36:21 +0800

    添加中间件配置和在路由中使用

--- app/middleware/logger.ts
+++ app/middleware/logger.ts
@@ -1,12 +1,19 @@
 // generate egg log middleware
-import { Context } from 'egg';
-import { appendFileSync } from 'fs';
-export default () => {
+import { Context, Application } from 'egg'
+import { appendFileSync } from 'fs'
+export default (options: any, app: Application) => {
   return async (ctx: Context, next: () => Promise<any>) => {
-    const start = Date.now();
-    const requestTime = new Date();
-    await next();
-    const used = Date.now() - start;
-    appendFileSync('./log.txt', `request as ${requestTime} ${ctx.method} ${ctx.url} ${used}ms\n`);
-  };
+    console.log('options', options)
+    console.log('default options', app.config.logger)
+    const start = Date.now()
+    const requestTime = new Date()
+    await next()
+    const used = Date.now() - start
+    if (options && options.allowedMethods.includes(ctx.method)) {
+      appendFileSync(
+        './log.txt',
+        `request as ${requestTime} ${ctx.method} ${ctx.url} ${used}ms\n`
+      )
+    }
+  }
 }
--- app/router.ts
+++ app/router.ts
@@ -1,10 +1,12 @@
-import { Application } from 'egg';
+import { Application } from 'egg'
 
 export default (app: Application) => {
-  const { router, controller } = app;
+  const { router, controller } = app
 
-  router.get('/', controller.home.index);
-  router.get('/test/:id', controller.test.index);
-  router.post('/test/:id', controller.test.index);
-  router.get('/dog', controller.test.getDog);
-};
+  const logger = app.middleware.logger({ allowedMethods: ['GET'] }, app)
+
+  router.get('/', controller.home.index)
+  router.get('/test/:id', controller.test.index)
+  router.post('/test/:id', controller.test.index)
+  router.get('/dog', logger, controller.test.getDog)
+}
