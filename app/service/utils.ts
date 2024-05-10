dcac52997df2f5c82abb8e8f23455ee6aa32bf99
liujie <572890972@qq.com>
2024-05-10 16:29:08 +0800

    添加 body 样式

--- app/service/utils.ts
+++ app/service/utils.ts
@@ -2,6 +2,9 @@ import { Service } from 'egg'
 import { createSSRApp } from 'vue'
 import LogoComponents from 'lego-components'
 import { renderToString, renderToNodeStream } from '@vue/server-renderer'
+import testContentData from '../../testWork.json'
+// TODO 调试 为什么自己的不行
+import JieCliComponent from 'jie-cli-components'
 
 export default class UtilsService extends Service {
   async renderToPageData(query: { id: number; uuid: string }) {
@@ -10,6 +13,7 @@ export default class UtilsService extends Service {
       throw new Error('work not found')
     }
     const { title, desc, content } = work
+    // let content = testContentData
     const vueApp = createSSRApp({
       data() {
         return {
@@ -20,10 +24,24 @@ export default class UtilsService extends Service {
     })
     vueApp.use(LogoComponents)
     const html = await renderToString(vueApp)
+    const bodyStyle = this.propsToStyle(content && content.props)
     return {
       html,
       title,
       desc,
+      bodyStyle,
     }
+  }
+  propsToStyle(props = {}) {
+    const keys = Object.keys(props)
+    const styleArr = keys.map((key) => {
+      const formatKey = key.replace(
+        /[A-Z]/g,
+        (match) => `-${match.toLowerCase()}`
+      )
+      const value = props[key]
+      return `${formatKey}:${value}`
+    })
+    return styleArr.join(';')
   }
 }
