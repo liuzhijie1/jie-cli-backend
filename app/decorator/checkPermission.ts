ed161270c5cef800a1d9d0e2c1420f02b9a8f5ea
liujie <572890972@qq.com>
2024-06-26 14:48:06 +0800

    添加 casl 校验第一部分呢代码

--- app/decorator/checkPermission.ts
+++ app/decorator/checkPermission.ts
@@ -1,5 +1,14 @@
 import { Controller } from 'egg'
 import { GlobalErrorTypes } from '../error'
+import defineRoles from '../roles/roles'
+import { subject } from '@casl/ability'
+
+const caslMethodMapping: Record<string, string> = {
+  GET: 'read',
+  POST: 'create',
+  PATCH: 'update',
+  DELETE: 'delete',
+}
 
 export default function checkPermission(
   modelName: string,
@@ -14,12 +23,27 @@ export default function checkPermission(
       // @ts-ignore
       const { ctx } = that
       const { id } = ctx.params
-      const userId = ctx.state.user._id
-      const certianRecord = await ctx.model[modelName].findOne({ id })
-      if (
-        !certianRecord ||
-        certianRecord[userKey].toString() !== userId.toString()
-      ) {
+      // const userId = ctx.state.user._id
+      // const certianRecord = await ctx.model[modelName].findOne({ id })
+      const { method } = ctx.request
+      const action = caslMethodMapping[method]
+      if (!ctx.state && !ctx.state.user) {
+        return ctx.helper.error({ ctx, errorType })
+      }
+      let permission = false
+      const ability = defineRoles(ctx.state.user)
+      const rule = ability.relevantRuleFor(action, modelName)
+      if (rule && rule.conditions) {
+        const certianRecord = await ctx.model[modelName]
+          .findOne({
+            id,
+          })
+          .lean()
+        permission = ability.can(action, subject(modelName, certianRecord))
+      } else {
+        permission = ability.can(action, modelName)
+      }
+      if (!permission) {
         return ctx.helper.error({ ctx, errorType })
       }
       await originalMethod.apply(this, args)
